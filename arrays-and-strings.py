4c32adb9078e9033deb0989b3b4765df4b3c8c03
Guido de Giovanni <guidito.dg86@gmail.com>
2022-03-27 13:43:41 -0300

    Ex 2

--- arrays-and-strings.py
+++ arrays-and-strings.py
@@ -8,7 +8,14 @@ def isUnique (str_to_analyze):
         return False
 
 
-
+def isPermutation (string1, string2):
+    chars_in_1 = {}
+    for letter in string1:
+        chars_in_1[letter] = chars_in_1.get(letter, 0) + 1
+    chars_in_2 = {}
+    for letter in string2:
+        chars_in_2[letter] = chars_in_2.get(letter, 0) + 1
+    return chars_in_1 == chars_in_2
 
 
 class TestsForChapter(unittest.TestCase):
@@ -19,6 +26,16 @@ class TestsForChapter(unittest.TestCase):
         self.assertTrue(isUnique("abcdefghijklmnopqrst123"))
         self.assertFalse(isUnique("bedroom"))
         self.assertFalse(isUnique("abcdefrghijklmnopqrst123"))
+
+    def test_isPermutation(self):
+        self.assertTrue(isPermutation("qquestion", "stqionque"))
+        self.assertTrue(isPermutation("dog" , "god"))
+        self.assertTrue(isPermutation("velez" , "velez"))
+        self.assertTrue(isPermutation("abcdefghijklmnopqrst123","lambcndoefgpqrsthij12k3"))
+        self.assertFalse(isPermutation("bedroom", "houses1"))
+        self.assertFalse(isPermutation("abcdefrghijklmnopqrst123", "abcdefrghijklmgnopqrst123"))
+        self.assertFalse(isPermutation("notebook", "desktop"))
+        self.assertFalse(isPermutation("notebook", "booknoti"))
 
 
 if __name__ == '__main__':
